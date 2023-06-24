832b9af423eb8d3d63a36f39f5fbbf240b0b5dff
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-06-24 22:08:12 +0530

    Update CalculatorTest.java

--- src/test/java/com/training/CalculatorTest.java
+++ src/test/java/com/training/CalculatorTest.java
@@ -1,12 +1,21 @@
 package com.training;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
 	@Test
-	void test() {
-		
+	void testMultiply() {
+		Calculator calculator = new Calculator();
+		assertEquals(6, calculator.multiply(2, 3));
+	}
+	
+	@Test
+	void testMultiplyNotValid() {
+		Calculator calculator = new Calculator();
+		assertEquals(0, calculator.multiply(2, 3));
 	}
 
 }
