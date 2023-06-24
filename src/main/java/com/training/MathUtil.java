f1566cd47a82931a1ef9b2d7fe240b7068d8de43
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-06-24 22:51:34 +0530

    update

---
+++ src/main/java/com/training/MathUtil.java
@@ -0,0 +1,21 @@
+package com.training;
+
+public class MathUtil {
+
+	public static Integer add(int i, int j) {
+		return i+j;
+	}
+
+	public static Integer multiple(int i, int j) {
+		return i*j;
+	}
+
+	public static Integer devide(int i, int j) {
+		return i/j;
+	}
+
+	public static boolean isEven(int i) {
+		return i%2==0;
+	}
+
+}
