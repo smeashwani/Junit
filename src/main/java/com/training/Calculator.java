3d92f61b70f1ba16a6c78fc50e46943e036e3216
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-06-24 22:03:19 +0530

    update

---
+++ src/main/java/com/training/Calculator.java
@@ -0,0 +1,7 @@
+package com.training;
+
+public class Calculator {
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+}
---
+++ src/test/java/com/training/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.training;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
